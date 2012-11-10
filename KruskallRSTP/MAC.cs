a177c015f34e774d9858e0615cb22c136e1f5635
Marek <mulcio@poczta.onet.pl>
2012-11-10 17:38:41 +0100

    start repo

---
+++ KruskallRSTP/MAC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KruskallRSTP {
+    class MAC {
+        private long first;
+        private long second;
+        private long third;
+        public MAC(long first, long second, long third) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+    }
+}
---
+++ KruskallRSTP/Port.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KruskallRSTP {
+    class Port {
+        public MAC mac { get; private set; }
+        public Port destinationPort { get; private set; }
+        public int Time { get; private set; }
+
+        private Queue<BPDU> bpdus;
+
+        public Port(MAC mac, Port destinationPort, int time) {
+            this.mac = mac;
+            this.destinationPort = destinationPort;
+            if (destinationPort != null) {
+                this.Time = time;
+            } else {
+                this.Time = int.MaxValue;
+            }
+
+            bpdus = new Queue<BPDU>();
+        }
+
+        private void receiveBPDU(BPDU bpdu) {
+            bpdus.Enqueue(bpdu);
+        }
+
+        public void sendBPDU(BPDU bpdu) {
+            if (destinationPort != null) {
+                destinationPort.receiveBPDU(bpdu);
+            }
+        }
+
+        public BPDU getBPDU() {
+            return bpdus.Dequeue();
+        }
+
+    }
+}
